98d0f7acc769ae0c51df126baa30b392cd9fb8d8
hemantpatil <hemantpatil@localhost>
2025-06-24 20:40:33 +0530

    First Commit...

---
+++ src/main/java/com/hemant/deployment/DeploymentCheckApplication.java
@@ -0,0 +1,13 @@
+package com.hemant.deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymentCheckApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hemant/deployment/MyRestController.java
@@ -0,0 +1,15 @@
+package com.hemant.deployment;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+	
+	
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return "Name is " + name;
+	}
+}
